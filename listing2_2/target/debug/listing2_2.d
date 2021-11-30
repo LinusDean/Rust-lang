d5e7109bfd8717ef708c5b1a48012ec0bf41daed
Dean <tingjeremypriv@gmail.com>
2021-11-30 22:59:23 +0800

    add integers using variables and declaring types

---
+++ listing2_2/target/debug/listing2_2.d
@@ -0,0 +1,1 @@
+/Users/dean/RustProject/Rust-lang/listing2_2/target/debug/listing2_2: /Users/dean/RustProject/Rust-lang/listing2_2/src/main.rs
---
+++ listing2_2/target/rls/debug/deps/listing2_2-511651c29e48cf18.d
@@ -0,0 +1,5 @@
+/Users/dean/RustProject/Rust-lang/listing2_2/target/rls/debug/deps/listing2_2-511651c29e48cf18.rmeta: src/main.rs
+
+/Users/dean/RustProject/Rust-lang/listing2_2/target/rls/debug/deps/listing2_2-511651c29e48cf18.d: src/main.rs
+
+src/main.rs:
